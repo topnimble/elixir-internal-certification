9116edf13ed5d94bb97a483eeafad5617300034f
topnimble <90754900+topnimble@users.noreply.github.com>
2022-11-10 16:23:28 +0700

    [#4] Add Nimble templates to dependencies

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule ElixirInternalCertification.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:nimble_template, "~> 4.4.0", only: :dev, runtime: false}
     ]
   end
 
