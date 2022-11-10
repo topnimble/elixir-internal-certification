9d89a2ee7e43485f651e8b126f0cc2ee18fe9005
topnimble <90754900+topnimble@users.noreply.github.com>
2022-11-10 18:07:37 +0700

    [#4] Fix command

--- build.sh
+++ build.sh
@@ -7,9 +7,7 @@ mix deps.get --only prod
 MIX_ENV=prod mix compile
 
 # Compile assets
-npm install --prefix ./assets
-npm run deploy --prefix ./assets
-mix phx.digest
+MIX_ENV=prod mix assets.deploy
 
 # Build the release and overwrite the existing release directory
 MIX_ENV=prod mix release --overwrite
