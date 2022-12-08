d57c1403ae4bba01062a2abb54cc7bc1baed1a68
topnimble <90754900+topnimble@users.noreply.github.com>
2022-12-08 20:21:22 +0700

    [#10] Add tests for keyword live

---
+++ test/elixir_internal_certification_web/live/keyword_live/keyword_live_test.exs
@@ -0,0 +1,26 @@
+defmodule ElixirInternalCertificationWeb.UploadLiveTest do
+  use ElixirInternalCertificationWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+
+  describe "LIVE /keywords" do
+    test "lists all keywords", %{conn: conn} do
+      user = insert(:user)
+      another_user = insert(:user)
+      insert(:keyword, user: user, title: "first keyword")
+      insert(:keyword, user: user, title: "second keyword")
+      insert(:keyword, user: user, title: "third keyword")
+      insert(:keyword, user: another_user, title: "another keyword")
+
+      conn = log_in_user(conn, user)
+
+      {:ok, _view, html} =
+        live(conn, Routes.keyword_path(ElixirInternalCertificationWeb.Endpoint, :index))
+
+      assert html =~ "first keyword"
+      assert html =~ "second keyword"
+      assert html =~ "third keyword"
+      refute html =~ "another keyword"
+    end
+  end
+end
