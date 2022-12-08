2ab24f1f293095f4674288cecbd2b41f10e5cf34
topnimble <90754900+topnimble@users.noreply.github.com>
2022-12-08 10:04:08 +0700

    [#12] [Backend] As a user, I can view the list of my uploaded keywords

--- test/elixir_internal_certification/keyword/keywords_test.exs
+++ test/elixir_internal_certification/keyword/keywords_test.exs
@@ -11,6 +11,46 @@ defmodule ElixirInternalCertification.Keyword.KeywordsTest do
 
   @fixture_path "test/support/fixtures"
 
+  describe "list_keywords/1" do
+    test "given a user, returns a list of keywords belongs to the user" do
+      user = insert(:user)
+      another_user = insert(:user)
+
+      insert(:keyword, user: user, title: "first keyword")
+      insert(:keyword, user: user, title: "second keyword")
+      insert(:keyword, user: another_user, title: "another keyword")
+
+      keywords = Keywords.list_keywords(user)
+
+      assert length(keywords) == 2
+
+      assert MapSet.equal?(
+               MapSet.new(Enum.map(keywords, fn keyword -> keyword.title end)),
+               MapSet.new(["first keyword", "second keyword"])
+             ) == true
+    end
+
+    test "given a user with NO keywords, returns an empty list" do
+      user = insert(:user)
+      another_user = insert(:user)
+
+      insert(:keyword, user: another_user, title: "another keyword")
+
+      keywords = Keywords.list_keywords(user)
+
+      assert length(keywords) == 0
+      assert keywords == []
+    end
+
+    test "given a user is nil, raises FunctionClauseError" do
+      user = nil
+
+      assert_raise FunctionClauseError, fn ->
+        Keywords.list_keywords(user)
+      end
+    end
+  end
+
   describe "create_keyword/2" do
     @valid_attrs %{title: "some title"}
     @invalid_attrs %{title: nil}
