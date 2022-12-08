48ae3eed4ad3f25ae7631b0cc82c599c83a22d59
topnimble <90754900+topnimble@users.noreply.github.com>
2022-12-08 19:50:22 +0700

    [#10] Add tests for upload live

---
+++ test/elixir_internal_certification_web/live/keyword_live/upload_live_test.exs
@@ -0,0 +1,108 @@
+defmodule ElixirInternalCertificationWeb.UploadLiveTest do
+  use ElixirInternalCertificationWeb.ConnCase, async: true
+
+  import Phoenix.LiveViewTest
+
+  setup [:register_and_log_in_user]
+
+  describe "LIVE /" do
+    test "uploads valid CSV file", %{conn: conn} do
+      {:ok, view, _html} =
+        live(conn, Routes.upload_path(ElixirInternalCertificationWeb.Endpoint, :index))
+
+      keyword =
+        file_input(view, "#upload-form", :keyword, [
+          %{
+            name: "keywords.csv",
+            content: "keyword\nfirst keyword\nsecond keyword\nthird keyword",
+            type: "text/csv"
+          }
+        ])
+
+      render_upload(keyword, "keywords.csv")
+
+      result =
+        view
+        |> element("#upload-form")
+        |> render_change()
+
+      assert result =~ "100%"
+    end
+
+    test "uploads INVALID file extension", %{conn: conn} do
+      {:ok, view, _html} =
+        live(conn, Routes.upload_path(ElixirInternalCertificationWeb.Endpoint, :index))
+
+      keyword =
+        file_input(view, "#upload-form", :keyword, [
+          %{
+            name: "invalid_file_extension.txt",
+            content: "keyword\nfirst keyword\nsecond keyword\nthird keyword",
+            type: "text/plain"
+          }
+        ])
+
+      render_upload(keyword, "invalid_file_extension.txt")
+
+      result =
+        view
+        |> element("#upload-form")
+        |> render_change()
+
+      assert result =~ "You have selected an unacceptable file type"
+    end
+
+    test "uploads too large file", %{conn: conn} do
+      {:ok, view, _html} =
+        live(conn, Routes.upload_path(ElixirInternalCertificationWeb.Endpoint, :index))
+
+      keyword =
+        file_input(view, "#upload-form", :keyword, [
+          %{
+            name: "keywords.csv",
+            content: "keyword\nfirst keyword\nsecond keyword\nthird keyword",
+            size: 999_999_999_999_999,
+            type: "text/csv"
+          }
+        ])
+
+      render_upload(keyword, "keywords.csv")
+
+      result =
+        view
+        |> element("#upload-form")
+        |> render_change()
+
+      assert result =~ "Too large"
+    end
+
+    test "uploads more than 1 file", %{conn: conn} do
+      {:ok, view, _html} =
+        live(conn, Routes.upload_path(ElixirInternalCertificationWeb.Endpoint, :index))
+
+      keyword =
+        file_input(view, "#upload-form", :keyword, [
+          %{
+            name: "keywords.csv",
+            content: "keyword\nfirst keyword\nsecond keyword\nthird keyword",
+            type: "text/csv"
+          },
+          %{
+            name: "keywords_2.csv",
+            content: "keyword\nfirst keyword\nsecond keyword\nthird keyword",
+            type: "text/csv"
+          }
+        ])
+
+      render_upload(keyword, "keywords.csv")
+      render_upload(keyword, "keywords_2.csv")
+
+      result =
+        view
+        |> element("#upload-form")
+        |> render_change()
+
+      assert result =~ "You have selected too many files"
+    end
+  end
+end
