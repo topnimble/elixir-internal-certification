535e4f758d1d2b0aab26fdbd725be257c299fc96
topnimble <90754900+topnimble@users.noreply.github.com>
2022-12-27 19:23:58 +0700

    [#14] Fix linting

--- lib/elixir_internal_certification/keyword/schemas/keyword.ex
+++ lib/elixir_internal_certification/keyword/schemas/keyword.ex
@@ -17,9 +17,7 @@ defmodule ElixirInternalCertification.Keyword.Schemas.Keyword do
     timestamps()
   end
 
-  def update_status_changeset(keyword \\ %__MODULE__{}, status) do
-    change(keyword, status: status)
-  end
+  def update_status_changeset(keyword \\ %__MODULE__{}, status), do: change(keyword, status: status)
 
   def changeset(%User{} = user, keyword \\ %__MODULE__{}, attrs) do
     keyword
