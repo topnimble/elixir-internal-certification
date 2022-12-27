b4ecbf665f1bda3403d77159a12bb0a2a3e18049
topnimble <90754900+topnimble@users.noreply.github.com>
2022-12-27 18:31:38 +0700

    [#14] Fix linting

--- lib/elixir_internal_certification/fetcher/google.ex
+++ lib/elixir_internal_certification/fetcher/google.ex
@@ -3,8 +3,11 @@ defmodule ElixirInternalCertification.Fetcher.Google do
 
   def search(query) do
     case GoogleClient.search(query) do
-      {:ok, %Tesla.Env{status: status_code, headers: headers, body: body}} -> {:ok, status_code, headers, body}
-      {:error, reason} -> {:error, reason}
+      {:ok, %Tesla.Env{status: status_code, headers: headers, body: body}} ->
+        {:ok, status_code, headers, body}
+
+      {:error, reason} ->
+        {:error, reason}
     end
   end
 end
