c6696480a99bd16889e0406eb2e36f5c8a4034d3
topnimble <90754900+topnimble@users.noreply.github.com>
2023-01-05 19:58:13 +0700

    [#15] Adjust UI

--- lib/elixir_internal_certification_web/router.ex
+++ lib/elixir_internal_certification_web/router.ex
@@ -27,9 +27,9 @@ defmodule ElixirInternalCertificationWeb.Router do
   scope "/", ElixirInternalCertificationWeb do
     pipe_through [:browser, :require_authenticated_user]
 
-    live "/", UploadLive.Index, :index
-    live "/keywords", KeywordLive.Index, :index
+    live "/", KeywordLive.Index, :index
     live "/keywords/:id", KeywordLive.Show, :show
+    live "/uploads", UploadLive.Index, :index
   end
 
   # Other scopes may use custom stacks.
