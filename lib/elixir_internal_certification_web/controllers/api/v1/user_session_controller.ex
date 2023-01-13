12a12a715f74d04842507c0da6f871bff84a6bb5
topnimble <90754900+topnimble@users.noreply.github.com>
2023-01-13 19:33:39 +0700

    [#18] [API] As a user, I can log in with my username and password

--- lib/elixir_internal_certification_web/controllers/api/v1/user_session_controller.ex
+++ lib/elixir_internal_certification_web/controllers/api/v1/user_session_controller.ex
@@ -10,11 +10,11 @@ defmodule ElixirInternalCertificationWeb.Api.V1.UserSessionController do
     if user = Accounts.get_user_by_email_and_password(email, password) do
       {:ok, token, _full_claims} = Guardian.encode_and_sign(user)
 
-      token = UserApiToken.new(%{token: token})
+      user_api_token = UserApiToken.new(%{token: token})
 
       conn
       |> put_status(:ok)
-      |> render("show.json", %{data: token})
+      |> render("show.json", %{data: user_api_token})
     else
       AuthErrorHandler.auth_error(conn, {:unauthenticated, :invalid_email_or_password}, [])
     end
---
+++ test/elixir_internal_certification_web/views/api/v1/user_session_view_test.exs
@@ -0,0 +1,14 @@
+defmodule ElixirInternalCertificationWeb.Api.V1.UserSessionViewTest do
+  use ElixirInternalCertificationWeb.ConnCase, async: true
+
+  alias ElixirInternalCertificationWeb.Api.V1.UserSessionView
+
+  describe "fields/0" do
+    test "returns a list of attribute fields" do
+      assert UserSessionView.fields() == [
+        :token,
+        :token_type
+      ]
+    end
+  end
+end
