21091a96cda4703f2bb614405f3324b68151cad3
topnimble <90754900+topnimble@users.noreply.github.com>
2023-01-18 10:42:40 +0700

    [#18] Refactor code to use `case`

--- lib/elixir_internal_certification_web/controllers/api/v1/user_session_controller.ex
+++ lib/elixir_internal_certification_web/controllers/api/v1/user_session_controller.ex
@@ -2,21 +2,23 @@ defmodule ElixirInternalCertificationWeb.Api.V1.UserSessionController do
   use ElixirInternalCertificationWeb, :controller
 
   alias ElixirInternalCertification.Account.Accounts
-  alias ElixirInternalCertification.Account.Schemas.UserApiToken
+  alias ElixirInternalCertification.Account.Schemas.{User, UserApiToken}
   alias ElixirInternalCertification.Guardian
   alias ElixirInternalCertificationWeb.AuthErrorHandler
 
   def create(conn, %{"email" => email, "password" => password}) do
-    if user = Accounts.get_user_by_email_and_password(email, password) do
-      {:ok, token, _full_claims} = Guardian.encode_and_sign(user)
+    case Accounts.get_user_by_email_and_password(email, password) do
+      %User{} = user ->
+        {:ok, token, _full_claims} = Guardian.encode_and_sign(user)
 
-      user_api_token = UserApiToken.new(%{token: token})
+        user_api_token = UserApiToken.new(%{token: token})
 
-      conn
-      |> put_status(:ok)
-      |> render("show.json", %{data: user_api_token})
-    else
-      AuthErrorHandler.auth_error(conn, {:unauthorized, :invalid_email_or_password}, [])
+        conn
+        |> put_status(:ok)
+        |> render("show.json", %{data: user_api_token})
+
+      _ ->
+        AuthErrorHandler.auth_error(conn, {:unauthorized, :invalid_email_or_password}, [])
     end
   end
 
