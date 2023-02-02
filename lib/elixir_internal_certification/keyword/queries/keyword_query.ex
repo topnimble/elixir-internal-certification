22cdd44d6cf441e331d53089ea8496c7a4434c62
topnimble <90754900+topnimble@users.noreply.github.com>
2023-02-02 12:09:21 +0700

    [#45] [Backend] As a user, I can find keywords using complex queries

--- lib/elixir_internal_certification/keyword/queries/keyword_query.ex
+++ lib/elixir_internal_certification/keyword/queries/keyword_query.ex
@@ -15,11 +15,19 @@ defmodule ElixirInternalCertification.Keyword.Queries.KeywordQuery do
         %User{id: user_id} = _user,
         search_query,
         search_query_type,
-        search_query_target
+        search_query_target,
+        number_of_occurrences,
+        symbol_notation
       ) do
     Keyword
     |> where([k], k.user_id == ^user_id)
-    |> maybe_filter_by_search_query_using_url(search_query, search_query_type, search_query_target)
+    |> maybe_filter_by_search_query_using_url(
+      search_query,
+      search_query_type,
+      search_query_target,
+      number_of_occurrences,
+      symbol_notation
+    )
     |> order_by([k], desc: k.id)
   end
 
@@ -28,73 +36,128 @@ defmodule ElixirInternalCertification.Keyword.Queries.KeywordQuery do
   defp maybe_filter_by_search_query(query, search_query),
     do: where(query, [k], ilike(k.title, ^"%#{search_query}%"))
 
-  defp maybe_filter_by_search_query_using_url(query, nil, _search_query_type, _search_query_target),
-    do: query
+  defp maybe_filter_by_search_query_using_url(
+         query,
+         nil,
+         _search_query_type,
+         _search_query_target,
+         _number_of_occurrences,
+         _symbol_notation
+       ),
+       do: query
 
   defp maybe_filter_by_search_query_using_url(
          query,
          search_query,
          search_query_type,
-         search_query_target
+         search_query_target,
+         number_of_occurrences,
+         symbol_notation
        ) do
     query
-    |> join_search_query(search_query_type)
-    |> where_search_query(search_query, search_query_type, search_query_target)
+    |> search_conditions(
+      search_query,
+      search_query_type,
+      search_query_target,
+      number_of_occurrences,
+      symbol_notation
+    )
     |> distinct(true)
   end
 
-  defp join_search_query(query, "partial_match") do
+  defp search_conditions(
+         query,
+         search_query,
+         "partial_match" = search_query_type,
+         "all" = _search_query_target,
+         number_of_occurrences,
+         _symbol_notation
+       ) do
+    query
+    |> join_search_query(search_query, search_query_type, number_of_occurrences)
+    |> where([_k, kl], ilike(kl.urls_of_adwords_advertisers_top_position, ^"%#{search_query}%"))
+    |> or_where([_k, kl], ilike(kl.urls_of_non_adwords, ^"%#{search_query}%"))
+  end
+
+  defp search_conditions(
+         query,
+         search_query,
+         "partial_match" = search_query_type,
+         "urls_of_adwords_advertisers_top_position" = _search_query_target,
+         number_of_occurrences,
+         _symbol_notation
+       ) do
+    query
+    |> join_search_query(search_query, search_query_type, number_of_occurrences)
+    |> where([_k, kl], ilike(kl.urls_of_adwords_advertisers_top_position, ^"%#{search_query}%"))
+  end
+
+  defp search_conditions(
+         query,
+         search_query,
+         "partial_match" = search_query_type,
+         "urls_of_non_adwords" = _search_query_target,
+         number_of_occurrences,
+         _symbol_notation
+       ) do
+    query
+    |> join_search_query(search_query, search_query_type, number_of_occurrences)
+    |> where([_k, kl], ilike(kl.urls_of_non_adwords, ^"%#{search_query}%"))
+  end
+
+  defp search_conditions(
+         query,
+         search_query,
+         "exact_match" = search_query_type,
+         "all" = _search_query_target,
+         number_of_occurrences,
+         _symbol_notation
+       ) do
+    query
+    |> join_search_query(search_query, search_query_type, number_of_occurrences)
+    |> where([_k, kl], ^search_query in kl.urls_of_adwords_advertisers_top_position)
+    |> or_where([_k, kl], ^search_query in kl.urls_of_non_adwords)
+  end
+
+  defp search_conditions(
+         query,
+         search_query,
+         "exact_match" = search_query_type,
+         "urls_of_adwords_advertisers_top_position" = _search_query_target,
+         number_of_occurrences,
+         _symbol_notation
+       ) do
+    query
+    |> join_search_query(search_query, search_query_type, number_of_occurrences)
+    |> where([_k, kl], ^search_query in kl.urls_of_adwords_advertisers_top_position)
+  end
+
+  defp search_conditions(
+         query,
+         search_query,
+         "exact_match" = search_query_type,
+         "urls_of_non_adwords" = _search_query_target,
+         number_of_occurrences,
+         _symbol_notation
+       ) do
+    query
+    |> join_search_query(search_query, search_query_type, number_of_occurrences)
+    |> where([_k, kl], ^search_query in kl.urls_of_non_adwords)
+  end
+
+  defp join_search_query(query, _search_query, "partial_match", _number_of_occurrences) do
     keyword_lookup_query =
       select(KeywordLookup, [kl], %{
         id: kl.id,
         keyword_id: kl.keyword_id,
         urls_of_adwords_advertisers_top_position:
-          fragment("UNNEST(urls_of_adwords_advertisers_top_position)"),
-        urls_of_non_adwords: fragment("UNNEST(urls_of_non_adwords)")
+          fragment("unnest(urls_of_adwords_advertisers_top_position)"),
+        urls_of_non_adwords: fragment("unnest(urls_of_non_adwords)")
       })
 
     join(query, :left, [k], kl in subquery(keyword_lookup_query), on: kl.keyword_id == k.id)
   end
 
-  defp join_search_query(query, "exact_match"),
+  defp join_search_query(query, _search_query, "exact_match", _number_of_occurrences),
     do: join(query, :left, [k], kl in assoc(k, :keyword_lookup))
-
-  defp where_search_query(query, search_query, "partial_match", "all") do
-    query
-    |> where([_k, kl], ilike(kl.urls_of_adwords_advertisers_top_position, ^"%#{search_query}%"))
-    |> or_where([_k, kl], ilike(kl.urls_of_non_adwords, ^"%#{search_query}%"))
-  end
-
-  defp where_search_query(
-         query,
-         search_query,
-         "partial_match",
-         "urls_of_adwords_advertisers_top_position"
-       ),
-       do:
-         where(
-           query,
-           [_k, kl],
-           ilike(kl.urls_of_adwords_advertisers_top_position, ^"%#{search_query}%")
-         )
-
-  defp where_search_query(query, search_query, "partial_match", "urls_of_non_adwords"),
-    do: where(query, [_k, kl], ilike(kl.urls_of_non_adwords, ^"%#{search_query}%"))
-
-  defp where_search_query(query, search_query, "exact_match", "all") do
-    query
-    |> where([_k, kl], ^search_query in kl.urls_of_adwords_advertisers_top_position)
-    |> or_where([_k, kl], ^search_query in kl.urls_of_non_adwords)
-  end
-
-  defp where_search_query(
-         query,
-         search_query,
-         "exact_match",
-         "urls_of_adwords_advertisers_top_position"
-       ),
-       do: where(query, [_k, kl], ^search_query in kl.urls_of_adwords_advertisers_top_position)
-
-  defp where_search_query(query, search_query, "exact_match", "urls_of_non_adwords"),
-    do: where(query, [_k, kl], ^search_query in kl.urls_of_non_adwords)
 end
