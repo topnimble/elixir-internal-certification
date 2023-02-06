87a8745a5f0cc034b88d5f2751664808b3bfa658
topnimble <90754900+topnimble@users.noreply.github.com>
2023-02-06 16:17:53 +0700

    [#45] [Backend] As a user, I can find keywords using complex queries

---
+++ test/elixir_internal_certification/keyword/schemas/advanced_search_test.exs
@@ -0,0 +1,34 @@
+defmodule ElixirInternalCertification.Account.Schemas.AdvancedSearchTest do
+  use ElixirInternalCertification.DataCase
+
+  alias Ecto.ULID
+
+  alias ElixirInternalCertification.Keyword.Schemas.AdvancedSearch
+
+  describe "new/1" do
+    test "given valid params, returns a valid changeset" do
+      expect(ULID, :generate, fn -> "01GQ45DK0QQEWQY6J01HV3BWQW" end)
+
+      assert AdvancedSearch.new(%{
+               search_query: "fi",
+               search_query_type: "partial_match",
+               search_query_target: "all",
+               number_of_occurrences: 0,
+               symbol_notation: ">"
+             }) == %AdvancedSearch{
+               id: "01GQ45DK0QQEWQY6J01HV3BWQW",
+               search_query: "fi",
+               search_query_type: "partial_match",
+               search_query_target: "all",
+               number_of_occurrences: 0,
+               symbol_notation: ">"
+             }
+    end
+
+    test "given MISSING params, raises FunctionClauseError" do
+      assert_raise FunctionClauseError, fn ->
+        AdvancedSearch.new(nil)
+      end
+    end
+  end
+end
